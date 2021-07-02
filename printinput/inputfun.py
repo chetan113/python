8f7b91ea780de9b428d68a3841f597ba2aecb657
cheta <cheta@LAPTOP-615E177F.Home>
2021-07-02 20:10:27 +0100

    inputfunctions

---
+++ printinput/inputfun.py
@@ -0,0 +1,11 @@
+"""s=input()
+print(s)
+
+s=input("enter ur name:")
+s1=input("enter ur surename:")
+
+print(s,s1)"""
+
+i=float(input("enter a integer numner:"))
+print(i)
+print(type(i))
---
+++ printinput/printfun.py
@@ -0,0 +1,12 @@
+print()
+print("hello")
+print("hello"*3)
+print("chetan \nbabu \nsakamuri")
+
+
+
+name="chetan"
+marks=94.9845
+print(name,marks)
+print("name is ",name ,"\n","marks are ",marks)
+print("name is {},marks are {} ".format(name,marks))
